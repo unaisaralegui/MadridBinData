dac7d7cda0b4705961f070c7a6a1753fe947e5e4
Saralegui Vallejo, Unai <unai.saralegui@tecnalia.com>
2021-07-12 14:59:41 +0200

    update postprocessing script

--- postprocessing.py
+++ postprocessing.py
@@ -1,18 +1,17 @@
 import sys
 import requests
+import json
 import pandas as pd
 
 # Simple script to show the parameters sent to the script, and generate a dummy file
 
-def update_data(existing_data, new_data_link):
+def update_data(existing_data, new_data_path):
     if existing_data is not None:
         max_date = existing_data['fecha'].max()
     else:
         max_date = None
-    try:
-        new_data = requests.get(new_data_link).json()
-    except Exception as ex:
-        return None
+    with open(new_data_path, 'r') as f:
+        new_data = json.load(new_data_path)
     new_dataf = pd.DataFrame(new_data['papeleras'])
     if max_date is not None:
         new_dataf_filtered = new_dataf.loc[new_dataf['fecha'] > max_date]
@@ -24,13 +23,12 @@ def update_data(existing_data, new_data_link):
 
 if __name__ == "__main__":
     filename = 'data.csv'
-    link = 'https://datos.madrid.es/datosabiertos/PAPELERASCOMPACTADAS/PAP_MAD_DA.json'
-    print("argv :", sys.argv)
+    new_data_path = sys.argv[1]
     try:
         existing_data = pd.read_csv(filename)
     except:
         existing_data = None
-    new_data = update_data(existing_data=existing_data, new_data_link=link)
+    new_data = update_data(existing_data=existing_data, new_data_path=new_data_path)
     if new_data is not None:
         print(f"Adding {len(new_data)} new rows")
         if existing_data is not None:
